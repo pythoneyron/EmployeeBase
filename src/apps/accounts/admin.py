d29a1ce0ed8ee0e35aa742ec1e17580a05532591
Платонов Олег <o.platonov@ddemo.ru>
2019-05-04 18:52:22 +0500

    Added filter and edit data in admin page

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -5,4 +5,14 @@ from apps.accounts.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name', 'phone_number', 'position', 'section')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        ('Important dates', {'fields': ('last_login', 'date_birth', 'start_date', 'end_date')}),
+    )
+
+    list_display = ('email', 'last_name', 'first_name', 'section', 'is_active')
+    search_fields = ('id', 'email', 'first_name', 'last_name', )
+    list_filter = ('is_active', 'section')
+    list_editable = ('is_active',)
