5c17a54b5e6add7b20f6650d1ec20e8e74de600b
Платонов Олег <o.platonov@ddemo.ru>
2019-05-11 20:25:42 +0500

    Config for host PythonAnywhere

--- src/settings/env/production.py
+++ src/settings/env/production.py
@@ -3,18 +3,29 @@ import os
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*',
+    'pythoneyron.pythonanywhere.com',
 ]
 
 SITE_HOST = ''
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('POSTGRES_DB'),
+#         'USER': os.getenv('POSTGRES_USER'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('POSTGRES_HOST'),
+#         'PORT': os.getenv('POSTGRES_PORT'),
+#     }
+# }
+
+# settings for host PythonAnywhere
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pythoneyron$employeebase_db',
+        'USER': 'pythoneyron',
+        'PASSWORD': 'Ajdnc67Jci702JCqcb',
+        'HOST': 'pythoneyron.mysql.pythonanywhere-services.com',
     }
 }
